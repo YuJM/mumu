aa99f20741509032edefb600010343b3adad23da
YuJM <polcaneli@gmail.com>
2020-02-03 21:59:15 +0900

    bazel 스크립트

--- packages/ui/button/pa-button/pa-button.component.ts
+++ packages/ui/button/pa-button/pa-button.component.ts
@@ -1,15 +1,22 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, ViewEncapsulation} from '@angular/core';
+
+const className = 'pa-button';
 
 @Component({
   selector: 'app-pa-button',
   templateUrl: './pa-button.component.html',
-  styleUrls: ['./pa-button.component.scss']
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class PaButtonComponent implements OnInit {
- @Input() text;
-  constructor() { }
+  @Input() text;
+
+  constructor(private _hostElementRef: ElementRef) {
+
+  }
 
   ngOnInit(): void {
+    this._hostElementRef.nativeElement.classList.add(className);
   }
 
 }
