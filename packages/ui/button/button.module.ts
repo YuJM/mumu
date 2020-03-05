450e75e984e10529efac450a1f8d10d4fd475eca
YuJM <polcaneli@gmail.com>
2020-03-05 23:55:19 +0900

    common 패키지 모듈 빌드 성공

--- packages/ui/button/button.module.ts
+++ packages/ui/button/button.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
 import {PaButtonComponent} from './pa-button.component';
+import {CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [PaButtonComponent],
-  imports: [
-  ],
+  imports: [CommonModule],
   exports: [PaButtonComponent]
 })
 export class PaButtonModule { }
